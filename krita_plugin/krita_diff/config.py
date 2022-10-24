b6b6d35148e10a5f8ebd94ba446cc41de8f91fc6
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2022-10-24 10:01:51 +0800

    add lock to config object

--- krita_plugin/krita_diff/config.py
+++ krita_plugin/krita_diff/config.py
@@ -1,12 +1,12 @@
 from dataclasses import asdict
 from typing import Any
 
-from krita import QSettings
+from krita import QObject, QReadWriteLock, QSettings
 
 from .defaults import CFG_FOLDER, CFG_NAME, DEFAULTS
 
 
-class Config:
+class Config(QObject):
     def __init__(self, folder=CFG_FOLDER, name=CFG_NAME, model=DEFAULTS):
         """Sorta like a controller for QSettings.
 
@@ -19,7 +19,10 @@ class Config:
             name (str, optional): Name of settings file. Defaults to CFG_NAME.
             model (Any, optional): Data model representing config & defaults. Defaults to DEFAULTS.
         """
+        # See: https://doc.qt.io/qt-6/qsettings.html#accessing-settings-from-multiple-threads-or-processes-simultaneously
+        # but im too lazy to figure out creating separate QSettings per worker, so we will just lock
         self.model = model
+        self.lock = QReadWriteLock()
         self.config = QSettings(QSettings.IniFormat, QSettings.UserScope, folder, name)
 
         # add in new config settings
@@ -39,11 +42,14 @@ class Config:
         Returns:
             Any: Config value.
         """
+        self.lock.lockForRead()
         # notably QSettings assume strings too unless specified
         assert self.config.contains(key) and hasattr(
             self.model, key
         ), "Report this bug, developer missed out a config key somewhere."
-        return self.config.value(key, type=type)
+        val = self.config.value(key, type=type)
+        self.lock.unlock()
+        return val
 
     def set(self, key: str, val: Any, overwrite: bool = True):
         """Set config value by key.
@@ -53,8 +59,10 @@ class Config:
             val (Any): Config value.
             overwrite (bool, optional): Whether to overwrite an existing value. Defaults to False.
         """
+        self.lock.lockForWrite()
         if overwrite or not self.config.contains(key):
             self.config.setValue(key, val)
+        self.lock.unlock()
 
     def restore_defaults(self, overwrite: bool = True):
         """Reset settings to default.
