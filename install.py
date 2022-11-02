ea9e2ac92c15f8ae4513752099ff83838748c028
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2022-11-02 19:00:52 +0800

    Auto-update script, fixes

---
+++ install.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+
+from launch import git, run
+
+REPO_LOCATION = Path(__file__).parent
+auto_update = bool(os.environ.get("AUTO_UPDATE", "True"))
+
+if auto_update:
+    print("[auto-sd-paint-ext] Attempting auto-update...")
+
+    try:
+        # current_hash = run(
+        #     f'"{git}" -C {REPO_LOCATION} rev-parse HEAD',
+        #     "[auto-sd-paint-ext] Get commit hash.",
+        # ).strip()
+
+        run(f'"{git}" -C {REPO_LOCATION} fetch', "[auto-sd-paint-ext] Fetch upstream.")
+
+        run(f'"{git}" -C {REPO_LOCATION} pull', "[auto-sd-paint-ext] Pull upstream.")
+    except Exception as e:
+        print("[auto-sd-paint-ext] Auto-update failed:")
+        print(e)
+        print("[auto-sd-paint-ext] Ensure git was used to install extension.")
+
+# NOTE: if we ever get dependencies, we can install them here.
