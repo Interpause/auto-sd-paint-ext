6d92a86f745a3ea42e4f837b68a71cb8dd393d5d
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2022-12-20 22:11:54 +0800

    fix dropdown

--- frontends/krita/krita_diff/widgets/combo_box.py
+++ frontends/krita/krita_diff/widgets/combo_box.py
@@ -13,11 +13,16 @@ class QOptionValidator(QValidator):
         self.opts = opts
 
     def validate(self, input, pos):
-        if input in self.opts:
+        # Below validation rules make it impossible to type invalid options
+        if len(self.opts) < 2:
+            # List hasn't loaded yet
+            return QValidator.Intermediate, input, pos
+        elif input in self.opts:
             return QValidator.Acceptable, input, pos
         elif any(o.find(input) == 0 for o in self.opts):
             return QValidator.Intermediate, input, pos
-        return QValidator.Invalid, input, pos
+        else:
+            return QValidator.Invalid, input, pos
 
     def fixup(self, input):
         return ""
@@ -78,9 +83,16 @@ class QComboBoxLayout(QHBoxLayout):
         # prevent dropdown from closing when cfg_init is called by update
         if set(opts) != self._items:
             self._items = set(opts)
+            # as using editable mode, text isn't affected by clearing options
             self.qcombo.clear()
             self.qcombo.addItems(opts)
             self.qcombo.setValidator(QOptionValidator(self._items))
 
+        # avoid resetting the auto-completer
+        if self.qcombo.currentText() != self.cfg(self.selected_cfg):
+            self.qcombo.setEditText(self.cfg(self.selected_cfg))
+
     def cfg_connect(self):
-        self.qcombo.currentTextChanged.connect(partial(self.cfg.set, self.selected_cfg))
+        # Possible to get invalid by backspacing after selecting option
+        # but no one would do that deliberately
+        self.qcombo.editTextChanged.connect(partial(self.cfg.set, self.selected_cfg))
