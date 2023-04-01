da1507fe9eb5fa0548789b336d5554730d848649
JasonS09 <jason_segnini@hotmail.com>
2023-04-01 11:00:16 -0600

    Added the ability to paste copied image to the image loader layout.

--- frontends/krita/krita_diff/widgets/image_loader.py
+++ frontends/krita/krita_diff/widgets/image_loader.py
@@ -1,4 +1,4 @@
-from krita import QWidget, QFileDialog, QPixmap, QPushButton, QVBoxLayout, QHBoxLayout, Qt
+from krita import QApplication, QFileDialog, QPixmap, QPushButton, QVBoxLayout, QHBoxLayout, Qt
 from ..widgets import QLabel
 
 class ImageLoaderLayout(QVBoxLayout):
@@ -8,29 +8,35 @@ class ImageLoaderLayout(QVBoxLayout):
         self.preview = QLabel()
         self.preview.setAlignment(Qt.AlignCenter)
         self.importBtn = QPushButton('Import image')
+        self.pasteBtn = QPushButton('Paste image')
         self.clearBtn = QPushButton('Clear')
 
         btnLayout = QHBoxLayout()
         btnLayout.addWidget(self.importBtn)
-        btnLayout.addWidget(self.clearBtn)
+        btnLayout.addWidget(self.pasteBtn)
 
         self.addLayout(btnLayout)
+        self.addWidget(self.clearBtn)
         self.addWidget(self.preview)
 
         self.importBtn.released.connect(self.load_image)
+        self.pasteBtn.released.connect(self.paste_image)
         self.clearBtn.released.connect(self.clear_image)
 
     def disable(self):
         self.preview.setEnabled(False)
         self.importBtn.setEnabled(False)
+        self.pasteBtn.setEnabled(False)
         self.clearBtn.setEnabled(False)
 
     def enable(self):
         self.preview.setEnabled(True)
         self.importBtn.setEnabled(True)
+        self.pasteBtn.setEnabled(True)
         self.clearBtn.setEnabled(True)
 
     def load_image(self):
+        self.clear_image()
         file_name, _ = QFileDialog.getOpenFileName(self.importBtn, 'Open File', '', 'Image Files (*.png *.jpg *.bmp)')
         if file_name:
             pixmap = QPixmap(file_name)
@@ -39,6 +45,10 @@ class ImageLoaderLayout(QVBoxLayout):
                 pixmap = pixmap.scaledToWidth(self.preview.width(), Qt.SmoothTransformation)
                 
             self.preview.setPixmap(pixmap)
+
+    def paste_image(self):
+        self.clear_image()
+        self.preview.setPixmap(QApplication.clipboard().pixmap())
 
     def clear_image(self):
         self.preview.setPixmap(QPixmap())
