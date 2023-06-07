cdc2029cd6da13d523087dd937be4d178fd27e09
drhead <1313496+drhead@users.noreply.github.com>
2023-06-07 19:26:01 -0400

    Implement upscale postprocess API call function

--- frontends/krita/krita_diff/client.py
+++ frontends/krita/krita_diff/client.py
@@ -278,7 +278,7 @@ class Client(QObject):
             tiling=tiling,
             restore_faces=self.cfg("face_restorer_model", str) != "None",
             override_settings=self.options_params(),
-            override_settings_restore_afterwards=False,
+            override_settings_restore_afterwards=True,
             alwayson_scripts={}
         )
 
@@ -639,6 +639,35 @@ class Client(QObject):
             else {"src_img": img_to_b64(src_img)}
         )
         self.post("upscale", params, cb)
+
+    def post_official_api_upscale_postprocess(self, cb, src_imgs: list, width, height):
+        """Uses official API. Intended for finalizing img2img pipeline."""
+
+        params = dict(
+            resize_mode=1,
+            show_extras_results=False,
+            gfpgan_visibility=0,
+            codeformer_visibility=0,
+            codeformer_weight=0,
+            upscaling_resize=1,
+            upscaling_resize_w=width,
+            upscaling_resize_h=height,
+            upscaling_crop=True,
+            upscaler_1=self.cfg("upscaler_name", str),
+            upscaler_2="None", # Todo: would be nice to support blended upscalers
+            extras_upscaler_2_visibility=0,
+            upscale_first=False,
+            imageList=[]
+        )
+
+        for img in src_imgs:
+            params["imageList"].append({
+                "data": img,
+                "name": "example_image"
+            })
+
+        url = get_url(self.cfg, prefix=OFFICIAL_ROUTE_PREFIX)
+        self.post("extra-batch-images", params, cb, base_url=url)
 
     def post_controlnet_preview(self, cb, src_img):
         unit = self.cfg("controlnet_unit", str)
